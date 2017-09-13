b8e85f286c63f83232b1fa2661b71a4329f8be12
Meng-Gen Tsai <plover@gmail.com>
2017-09-13 22:28:39 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+my-hibernate-app
+================
+- $ mvn clean compile assembly:single
+- $ java -jar target/my-hibernate-app-1.0-SNAPSHOT-jar-with-dependencies.jar 
+
+
+# References:
+- https://www.boraji.com/hibernate-5-basic-configuration-example
---
+++ src/main/java/com/menggen/app/App.java
@@ -0,0 +1,21 @@
+package com.menggen.app;
+
+import org.hibernate.Session;
+
+public class App {
+    public static void main(String[] args) {
+	    Session session = HibernateUtil.getSessionFactory().openSession();
+	    session.beginTransaction();
+
+	    // Check database version
+	    String sql = "select version()";
+
+	    String result = (String) session.createNativeQuery(sql).getSingleResult();
+	    System.out.println(result);
+
+	    session.getTransaction().commit();
+	    session.close();
+
+	    HibernateUtil.shutdown();
+    }
+}
