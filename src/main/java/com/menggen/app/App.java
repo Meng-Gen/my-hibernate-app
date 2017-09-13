49df8760f2e4e3eb3d1ac61753d0ebb7d083d71d
Meng-Gen Tsai <plover@gmail.com>
2017-09-14 00:29:56 +0800

    Remove bad chars and format code

--- src/main/java/com/menggen/app/App.java
+++ src/main/java/com/menggen/app/App.java
@@ -3,21 +3,23 @@ package com.menggen.app;
 import org.hibernate.Session;
 
 import java.math.BigInteger;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-	    Session session = HibernateUtil.getSessionFactory().openSession();
-	    session.beginTransaction();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
 
-	    // Check database version
-	    String sql = "select count(1) from TASK_DETAILS;";
+        List<TaskDetails> taskDetailsList = (List<TaskDetails>)session.createQuery("from TaskDetails").list();
+        for (TaskDetails taskDetails : taskDetailsList) {
+            System.out.println(taskDetails.getId());
+            System.out.println(taskDetails.getKey());
+            System.out.println(taskDetails.getValue());
+        }
 
-	    BigInteger result = (BigInteger) session.createNativeQuery(sql).getSingleResult();
-	    System.out.println(result);
+        session.getTransaction().commit();
+        session.close();
 
-	    session.getTransaction().commit();
-	    session.close();
-
-	    HibernateUtil.shutdown();
+        HibernateUtil.shutdown();
     }
 }
